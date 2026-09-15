cd20336280e8f2d702b46864b376e34135d06077
poomani3066-mp <poomani3066@gmail.com>
2026-09-15 11:51:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI(title="NHS Digital Hospital Agent")
+
+
+class Appointment(BaseModel):
+    patient_name: str
+    date: str
+    time: str
+    department: str
+
+
+appointments: List[Appointment] = []
 
 
 @app.get("/")
@@ -15,4 +27,21 @@ def home():
 def health():
     return {
         "status": "healthy"
+    }
+
+
+@app.post("/appointments")
+def book_appointment(appointment: Appointment):
+    appointments.append(appointment)
+
+    return {
+        "message": "Appointment booked successfully",
+        "appointment": appointment
+    }
+
+
+@app.get("/appointments")
+def get_appointments():
+    return {
+        "appointments": appointments
     }
